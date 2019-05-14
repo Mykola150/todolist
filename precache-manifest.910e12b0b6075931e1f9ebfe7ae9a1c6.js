3b6239399459afafd6bf21168198fd78e6d2c510
Nick <vacanza@ukr.net>
2019-05-14 14:26:20 +0300

    Updates

---
+++ precache-manifest.910e12b0b6075931e1f9ebfe7ae9a1c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c781672e2a97d134ae5aa19171e2e94e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e97a3d73d304132d8313",
+    "url": "/todolist/static/css/main.2cbaef7e.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/todolist/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "e97a3d73d304132d8313",
+    "url": "/todolist/static/js/main.42263804.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
